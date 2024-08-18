605ce98303828e9c845beeda49ba3dea4aadff06
anaBeatrizWalker <anabiawalker@gmail.com>
2024-08-18 15:17:04 -0300

    Adicionando conceito de classe object

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 //Concretização da representação de uma pessoa
 Person p1 = new Person("Ana");
 p1.Age = 22;
-p1.IntroduceYourself();
+// p1.IntroduceYourself();
 
 CheckingAccount c1 = new CheckingAccount(1223, 1000);
 // c1.ShowBalance();
@@ -14,15 +14,20 @@ Student a1 = new Student("Gustavo");
 a1.Email = "gustavo@gustavo.com";
 a1.Age = 24;
 a1.Score = 10;
-a1.IntroduceYourself();
+// a1.IntroduceYourself();
 
 Teacher t1 = new Teacher();
 t1.Name = "Leonardo";
 t1.Email = "leonardo@leonardo.com";
 t1.Age = 45;
 t1.Salary = 8750;
-t1.IntroduceYourself();
+// t1.IntroduceYourself();
 
 Checking checkingAccount = new Checking();
 checkingAccount.Credit(500);
 // checkingAccount.ShowBalance();
+
+Computer computer = new Computer();
+computer.ToString();
+computer.GetHashCode();
+Console.WriteLine(computer.ToString());
